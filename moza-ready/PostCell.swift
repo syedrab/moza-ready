a2dc6e1c27af34dbc9766f5a9c0ec1e7375de81e
Syed Rab <syed.rab@veeva.com>
2017-07-17 18:01:31 -0400

    FeedVC UI Table

---
+++ moza-ready/PostCell.swift
@@ -0,0 +1,30 @@
+//
+//  PostCell.swift
+//  moza-ready
+//
+//  Created by Syed Rab on 7/17/17.
+//  Copyright © 2017 Moza. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
