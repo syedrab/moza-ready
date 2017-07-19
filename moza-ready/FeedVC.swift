5ee81b8b1d4e302e8ace03be7e629cee44720bd2
Syed Rab <syed.rab@veeva.com>
2017-07-19 09:42:36 -0400

    Getting firebase data

--- moza-ready/FeedVC.swift
+++ moza-ready/FeedVC.swift
@@ -20,6 +20,11 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
+            print(snapshot.value)
+        })
+        
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
         signOut.isUserInteractionEnabled = true
         signOut.addGestureRecognizer(tapGestureRecognizer)
