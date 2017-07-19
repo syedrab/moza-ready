28b71959713f678a5196a764dfa030c7af5a40b2
Syed Rab <syed.rab@veeva.com>
2017-07-19 11:33:05 -0400

    post function

--- moza-ready/FeedVC.swift
+++ moza-ready/FeedVC.swift
@@ -10,18 +10,23 @@ import UIKit
 import Firebase
 import SwiftKeychainWrapper
 
-class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var signOut: UIImageView!
+    @IBOutlet weak var addImage: CircleView!
     
     var posts = [Post]()
+    var imagePicker: UIImagePickerController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
         
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
             if let snapshot = snapshot.children.allObjects as? [DataSnapshot] {
@@ -36,9 +41,9 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             self.tableView.reloadData()
         })
         
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
-        signOut.isUserInteractionEnabled = true
-        signOut.addGestureRecognizer(tapGestureRecognizer)
+//        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+//        signOut.isUserInteractionEnabled = true
+//        signOut.addGestureRecognizer(tapGestureRecognizer)
     }
 
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -60,19 +65,20 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         }
         //return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
-    
-    func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
-    {
-        let tappedImage = tapGestureRecognizer.view as! UIImageView
-        let removeSuccessful: Bool = KeychainWrapper.standard.removeObject(forKey: KEY_UID)
-        print("MOZA: ID Removed from KeyChain - \(removeSuccessful)")
-        try! Auth.auth().signOut()
-        performSegue(withIdentifier: "goToSignIn", sender: nil)
-        
-        // Your action
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print("test")
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            addImage.image = image
+        } else {
+            print("MOZA: A valid image wasn't selected")
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func addImageTapped(_ sender: Any) {
+        present(imagePicker, animated: true, completion: nil)
     }
     
-    @IBAction func signOutTapped(_ sender: Any) {
+    @IBAction func signoutTapped(_ sender: Any) {
         let removeSuccessful: Bool = KeychainWrapper.standard.removeObject(forKey: KEY_UID)
         print("MOZA: ID Removed from KeyChain - \(removeSuccessful)")
         try! Auth.auth().signOut()
