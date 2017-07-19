6e00e8682f740bed108385dd1f2ebc1d130d06fa
Syed Rab <syed.rab@veeva.com>
2017-07-19 10:29:03 -0400

    firebase display data

--- moza-ready/FeedVC.swift
+++ moza-ready/FeedVC.swift
@@ -52,9 +52,13 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let post = posts[indexPath.row]
-        print("MOZA: \(post.caption)")
-        
-        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+            cell.configureCell(post: post)
+            return cell
+        }else {
+            return PostCell()
+        }
+        //return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
     
     func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
--- moza-ready/PostCell.swift
+++ moza-ready/PostCell.swift
@@ -15,10 +15,19 @@ class PostCell: UITableViewCell {
     @IBOutlet weak var postImage: UIImageView!
     @IBOutlet weak var caption: UITextView!
     @IBOutlet weak var likesLbl: UILabel!
+    
+    var post: Post!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func configureCell(post: Post) {
+        self.post = post
+        self.caption.text = post.caption
+        self.likesLbl.text = "\(post.likes)"
+        //self.postImage = post.imageUrl
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
